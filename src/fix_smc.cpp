c499a394751f53ae602ffee240523fa56625c020
Zheng Yang <zchoong001@e.ntu.edu.sg>
2023-06-18 23:22:58 +0800

    Bug fix

--- src/fix_smc.cpp
+++ src/fix_smc.cpp
@@ -342,7 +342,7 @@ void FixSMC::post_integrate() {
     double rand;
 
     for (int i = 0; i < smcnum; i++) {
-      for (int l_sample = 0; l_sample < abs(maxadir)){
+      for (int l_sample = 0; l_sample < abs(maxadir); l_sample ++){
 
       // roll new movement step size (left or right)
       // but.... should adir and hdir be independent? they should right
