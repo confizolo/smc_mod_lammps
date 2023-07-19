425e49b15dd1fb108556eb01818601c5f1cec5bb
Zheng Yang <zchoong001@e.ntu.edu.sg>
2023-07-19 22:12:26 +0800

    Bug fix

--- analysis/generate_scripts.py
+++ analysis/generate_scripts.py
@@ -17,7 +17,7 @@ def generate_master_lams_file():
 def main(do_local, do_slurm, jobname, nrep = 96, npara = 16, run_duration = 100000, n_stiff = [4, 8, 12, 28, 40], lp_stiff = [200], add_force = False, custom_masterfile = "", regenerate_stiff = False, use_long = False, equil = "", **kwargs):
 
 	def check_equil_in_folder(equil_folder, lp, lp_stiff, n_stiff, force):
-		target_file = "eq_lp{:d}-{:d}_nstiff-{:d}_f-{:.1f}.dat".format(lp, lp_stiff, n_stiff, force) 
+		target_file = "eq_lp{:d}-{:d}_nstiff-{:d}_f-{:.2f}.dat".format(lp, lp_stiff, n_stiff, force) 
 		target_path = os.path.join(equil_folder, target_file)
 
 		if os.path.exists(target_path):
