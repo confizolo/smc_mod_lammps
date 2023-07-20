a844c7d30426e22baa5fc0b94b8180e2beafe361
Zheng Yang <zchoong001@e.ntu.edu.sg>
2023-07-20 22:11:56 +0800

    Init

---
+++ analysis/check_crash.py
@@ -0,0 +1,42 @@
+import argparse 
+import subprocess
+from pathlib import Path
+import os
+
+def parse(target_folders, log_file):
+
+	DEFAULT_RATE_SMC = 100
+	DEFAULT_RUN_DURATION = 100000
+	DEFAULT_LPOL = 1000
+	print_rate = 100
+	expected_print_lines = DEFAULT_RUN_DURATION/print_rate
+
+	with open(log_file, "w") as f:
+
+		for target_folder in target_folders:
+			# folder_id = target_folder.split("/")[-3] # manual, will break
+			# folder_list = [(f.path, f.name) for f in os.scandir(target_folder) if f.is_dir()]
+
+			rep_folder_list = [str(f.parent.absolute()) for f in Path(os.path.join(target_folder)).rglob("smc_pos.txt")]
+
+			for _, rep in enumerate(rep_folder_list):
+				pos_file = os.path.join(rep, "smc_pos.txt")
+				result = subprocess.check_output(['wc', '-l', pos_file, ])
+				obs_lines = int(result.decode("utf-8").split(" ")[0])
+
+				if obs_lines < expected_print_lines:
+					print(rep)
+					f.write(rep + "\n")
+
+if __name__ == "__main__":
+	ap = argparse.ArgumentParser()
+	ap.add_argument("-i","--input_folder", nargs = "+")
+	ap.add_argument("-o","--output_folder")
+
+	args = ap.parse_args()
+	
+	parse(args.input_folder, args.output_folder)
+
+
+
+
