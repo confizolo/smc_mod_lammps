150d8b58ecf0bc5fbfe922699fd49d5311ece192
Zheng Yang <zchoong001@e.ntu.edu.sg>
2023-08-02 15:38:48 +0800

    Change folder naming convention

--- analysis/generate_scripts.py
+++ analysis/generate_scripts.py
@@ -138,7 +138,7 @@ def main(do_local, do_slurm, jobname, nrep = 96, npara = 16, run_duration = 1000
 			else:
 				master_molecule_file = generate_stiff(_n_stiff, os.path.join(master_folder, "stiff_molecules"), default_paths[path_str]["molecule_file"], force = regenerate_stiff, extend_boundary=extend_boundary)
 
-			lp_folder = os.path.join(master_folder, jobname, f"N{lpol}", f"lp{lp:02}", f"lp-stiff{_lp_stiff:02}", f"n-stiff{_n_stiff:d}", f"force_{_force:.2f}")
+			lp_folder = os.path.join(master_folder, jobname, f"N{lpol}", f"lp{lp:02}", f"lp-stiff{_lp_stiff:02}", f"n-stiff{_n_stiff:d}", f"force_{_force:.2f}", "tan_{_tangent_cutoff:.2f}")
 
 			_start_position = int((lpol - _n_stiff)/2 - start_shift) # ??? e.g. for 1000 - 100, start at 430, move until 450
 
