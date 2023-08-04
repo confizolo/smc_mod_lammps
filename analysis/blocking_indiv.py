7ab86dc6568f6abaf140693b016f5117fb47bd3d
Zheng Yang <zchoong001@e.ntu.edu.sg>
2023-08-04 23:46:16 +0800

    Bug fix

--- analysis/blocking_indiv.py
+++ analysis/blocking_indiv.py
@@ -86,7 +86,7 @@ def parse(target_folders, output_file, run_time = 100000):
 					if _x != last_x:
 						last_x = _x
 						last_t = _t
-			if last_t < run_time:
+			if _t < run_time:
 				continue
 			params["t0"] = t0_candidate
 			params["dt"] = dt
