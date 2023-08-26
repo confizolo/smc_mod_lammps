430b86ea13d9cafd72c395167d6f07ccd11b0c3a
Zheng Yang <zchoong001@e.ntu.edu.sg>
2023-08-26 19:12:30 +0800

    Bug fix

--- analysis/generate_scripts.py
+++ analysis/generate_scripts.py
@@ -322,7 +322,7 @@ if __name__ == "__main__":
 
 	ap.add_argument("-cut", "--cutoff", default = 12., type = float)
 	ap.add_argument("-dt","--ratesmc", type = int, default = 1000)
-	ap.add_argument("-lf", "--langfric", default = 1.0)
+	ap.add_argument("-lf", "--langfric", default = 1.0, type = float)
 	ap.add_argument("job_name") # generate the SBATCH script
 
 	args = ap.parse_args()
